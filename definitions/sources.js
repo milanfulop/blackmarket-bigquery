a2f9a458963751decad21f33221fe3b6cac58494
Fülöp Milán <fulopmilan170@gmail.com>
2026-02-23 22:25:33 +0000

    feat: facts and dimensions, no date_dim implementation

---
+++ definitions/sources.js
@@ -0,0 +1,29 @@
+declare({
+  schema: "raw_oltp",
+  name: "orders"
+})
+
+declare({
+  schema: "raw_oltp",
+  name: "users"
+})
+
+declare({
+  schema: "raw_oltp",
+  name: "payments"
+})
+
+declare({
+  schema: "raw_oltp",
+  name: "escrows"
+})
+
+declare({
+  schema: "raw_oltp",
+  name: "user_sessions"
+})
+
+declare({
+  schema: "raw_oltp",
+  name: "fraud_flags"
+})
